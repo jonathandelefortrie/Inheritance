2f1b1b343b7f16a5d48a1d7cdd157f342ffaffe5
Jonathan Delefortrie <jonathandelefortrie@gmail.com>
2015-07-11 12:43:06 +0100

    first commit

---
+++ bicycle.js
@@ -0,0 +1,36 @@
+(function() {
+
+    'use strict';
+
+	var Base = Fry.Base;
+	var Class = Fry.Class;
+
+    function Bicycle() {
+
+        Bicycle.superclass.constructor.apply(this, arguments);
+    }
+
+    Bicycle.prototype = Class.extend(Class.create(Base), {
+
+        constructor: Bicycle,
+
+        init: function(config) {
+
+            console.log('Bicycle: init :', config);
+        },
+
+        hello: function() {
+
+            console.log('Bicycle: hello');
+        },
+
+        destroy: function() {
+
+            console.log('Bicycle: destroy');
+        }
+
+    });
+
+    Fry.Bicycle = Bicycle;
+
+})();
